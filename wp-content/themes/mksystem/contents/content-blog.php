beb82149ef13e62def781806022dda63d8965c9f
Julio Maturrano <julio16lb@gmail.com>
2016-11-20 14:44:01 -0500

    creacion de page blog

--- wp-content/themes/mksystem/contents/content-blog.php
+++ wp-content/themes/mksystem/contents/content-blog.php
@@ -2,22 +2,20 @@
 /**
  * The template used for displaying page content in page-blog.php
  *
- * @package dazzling
+ * @package devit
  */
 ?>
 
-
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header page-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-BLOG
-	</header><!-- .entry-header -->
-	<div class="entry-content col-md-12">
-	<div class="row2">
-
-
-	</div><!-- .entry-content -->
-	<?php edit_post_link( __( 'Edit', 'dazzling' ), '<footer class="entry-meta"><i class="fa fa-pencil-square-o"></i><span class="edit-link">', '</span></footer>' ); ?>
-
-	</header>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header page-header">
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+    </header><!-- .entry-header -->
+    <div class="entry-content">
+        <div class="col-md-7 text-justify">
+            <?php the_content(); ?>
+        </div>
+        <div class="col-md-5">
+            <?php the_post_thumbnail( 'devitweb', array( 'class' => 'thumbnail' )); ?>
+        </div>
+    </div>
 </article><!-- #post-## -->
