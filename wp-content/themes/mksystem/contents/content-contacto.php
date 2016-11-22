abb8e9bc581dc5024b5fc8456382240f2e41bb4b
mkdesarrollo <desarrollo@mksystemgroup.com>
2016-11-22 12:59:26 -0500

    contacto formulario -estilo

--- wp-content/themes/mksystem/contents/content-contacto.php
+++ wp-content/themes/mksystem/contents/content-contacto.php
@@ -8,10 +8,10 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="entry-header page-header">
-        <h1 class="entry-title"><?php the_title(); ?></h1>
+        <h1 class="entry-title text-center"><?php the_title(); ?></h1>
     </header><!-- .entry-header -->
     <div class="entry-content">
-        <div class="col-md-7 text-justify">
+        <div class="col-md-12 text-center">
             <?php the_content(); ?>
         </div>
         <div class="col-md-5">
